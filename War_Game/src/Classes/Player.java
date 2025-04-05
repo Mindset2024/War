13311257afebed40d158c4dde1f43d66239741dc
Tyler <dgaf.mindset.2025@outlook.com>
2025-04-05 10:51:49 -0600

    Update Player.java

--- War_Game/src/Classes/Player.java
+++ War_Game/src/Classes/Player.java
@@ -7,7 +7,7 @@ public class Player {
     private int score;
     private String name;
 
-    // Corrected constructor (no typos)
+
     public Player(String name) {
         this.name = name;
         this.score = 0;
